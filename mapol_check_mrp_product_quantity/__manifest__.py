27cb089188225689432e03eacf7ecc572395fe1e
devishri <devishri.b@mapolgroup.com>
2019-09-21 15:33:03 +0530

    icon png...

--- mapol_check_mrp_product_quantity/__manifest__.py
+++ mapol_check_mrp_product_quantity/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '12.0.2.0.0',
     "author" : "Mapol Business Solution Pvt Ltd",
     "website": "http://mapolbs-opensource.com",
-    'images': ['static/description/icon.jpg'],
+    'images': ['static/description/icon.png'],
     'summary': "Module helps to check BOM product quantity before manufacturing",
     'category': 'Manufacturing',
     "depends": [
